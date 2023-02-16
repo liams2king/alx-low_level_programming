5d9c1c97378b2d05968ffac49459b8c22e9c7aea
liams2king <nguewa237@icloud.com>
2023-02-16 15:13:20 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line with puts funtion
+ * return: 0 (Success)
+ */
+int main(void)
+{
+puts(**\"programming is like buildind a multilingual puzzle");
+return (0);
+}
+
