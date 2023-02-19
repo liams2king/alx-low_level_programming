50077d17ceefd36628752e5e2b6ea26903cb7f55
liams2king <nguewa237@icloud.com>
2023-02-18 22:41:01 -0800

    .c:12: ERROR: space required before the open parenthesis '('

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,22 +2,25 @@
 #include <time.h>
 /* This program will assign a random number to the variable n */
 
-/* betty style doc for function main goes there */
+/* Return: Always 0 (Success) */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n > 0)
-		printf("n is positive\n" )
-	} else {
-	if(n = 0)
-		printf("n is zero\n" )
-	} else {
-	if(n < 0)
-		printf("n is negatif\n" )
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
 
-	/* your code goes there */
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
 	return (0);
 }
