7a99dd8f8fb392757ca50496653e13163ce20fd6
liams2king <nguewa237@icloud.com>
2023-02-19 12:46:17 -0800

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,4 +1,4 @@
-#include ,stdio.h>
+#include <stdio.h>
 /**
  * main - Entry point
  *
@@ -11,7 +11,7 @@ int main(void)
 	c = 'z';
 	while
 		(c >= 'a') {
-			puchar(c);
+			putchar(c);
 			c--;
 		}
 	putchar('\n');
