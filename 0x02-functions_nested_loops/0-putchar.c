04f0150c06f360d3089ff94cfa1b0579d3a4fe75
liams2king <nguewa237@icloud.com>
2023-02-21 14:14:03 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - print _putchar
  * is written by mbah
@@ -6,12 +7,14 @@
  */
 
 int main(void)
+
 {
 _putchar('_');
 _putchar('p');
 _putchar('u');
 _putchar('t');
 _putchar('c');
+_putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -7,7 +7,7 @@
  * Return: on seccess 1.
  * on error, -1 is returned,and error is set  appropriately.
  */
-int _putchar(char)
+int _putchar(char c)
 {
-	return (write(1, &c, 1))
+	return (write(1, &c, 1));
 }
