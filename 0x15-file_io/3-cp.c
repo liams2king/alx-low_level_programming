046e7ab2c1c29b98129dad48d2cfcfba669694af
liams2king <nguewa237@icloud.com>
2023-06-15 14:30:22 -0700

    README

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define BUFFER_SIZE 1024
+
+/**
+ * main - Copies the content of a file to another file.
+ * @argc: The number of arguments passed.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success, or an exit status on failure.
+ */
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, num_bytes_read, num_bytes_written;
+	char buffer[BUFFER_SIZE];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+
+	while ((num_bytes_read = read(file_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		num_bytes_written = write(file_to, buffer, num_bytes_read);
+		if (num_bytes_written != num_bytes_read)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+
+	if (num_bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	if (close(file_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+
+	if (close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
+
+	return (0);
+}
+
