cc08529093f3c7ebe13a0a6c3bacc89bcfe8d546
liams2king <nguewa237@icloud.com>
2023-06-16 18:59:14 -0700

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 
 	if (idx == 0)
-		return add_dnodeint(h, n);
+		return (add_dnodeint(h, n));
 
 
 	while (temp != NULL && count < idx - 1)
@@ -30,7 +30,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 
 	if (temp->next == NULL)
-		return add_dnodeint_end(h, n);
+		return (add_dnodeint_end(h, n));
 
 
 	new_node = malloc(sizeof(dlistint_t));
